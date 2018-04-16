d88304de6014911da795fa67c4f0244de3b03b4d
saquibw <saquib.waheed@yahoo.com>
2018-04-16 21:07:36 +0600

    Initial commit

---
+++ src/main/java/com/bookcentric/app/BookcentricApplication.java
@@ -0,0 +1,12 @@
+package com.bookcentric.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcentricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcentricApplication.class, args);
+	}
+}
