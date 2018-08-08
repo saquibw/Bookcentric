446ebcdd74af5637f726f7c5977dfa08dbb68b5a
saquibw <saquib.waheed@yahoo.com>
2018-08-09 01:19:31 +0600

    Thymeleaf setup complete

--- src/main/java/com/bookcentric/component/user/UserController.java
+++ src/main/java/com/bookcentric/component/user/UserController.java
@@ -2,18 +2,28 @@ package com.bookcentric.component.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping(value="/user/add", method=RequestMethod.POST)
+	@GetMapping("/user/registration")
+	public ModelAndView viewRegistration(Model model) {
+		ModelAndView regView = new ModelAndView("user-registration");
+		
+		model.addAttribute("pageTitle", "Registration page");
+		
+		return regView;
+	}
+	
+	@PostMapping(value="/user/add")
 	public void addUser(@RequestBody User user) {
 
 		System.out.println(user.getFirstName());
