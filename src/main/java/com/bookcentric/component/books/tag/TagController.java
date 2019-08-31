8f9d774a7218027ea4b80dd3f746187ec86d670b
saquibw <saquib.waheed@yahoo.com>
2019-08-31 15:24:20 +0600

    Tag search mechanism implemented

--- src/main/java/com/bookcentric/component/books/tag/TagController.java
+++ src/main/java/com/bookcentric/component/books/tag/TagController.java
@@ -1,12 +1,16 @@
 package com.bookcentric.component.books.tag;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -44,5 +48,24 @@ public class TagController {
 		tagService.delete(tag);
 		
 		return "redirect:/tag/view";
+	}
+	
+	private List<Tag> sortByName(List<Tag> list) {
+		return list.stream().sorted(Comparator.comparing(Tag::getName)).collect(Collectors.toList());
+	}
+	
+	@ResponseBody
+	@GetMapping("/tag/get")
+	public List<Tag> getBySearchtext(@RequestParam String searchText) {
+		List<Tag> tagList = sortByName(tagService.findAll());
+		
+		if(!searchText.isEmpty()) {
+			if(tagList != null && tagList.size() > 0) {
+				tagList = tagList.stream()
+				.filter(tag -> tag.getName().toLowerCase().contains(searchText.toLowerCase()))
+				.collect(Collectors.toList());
+			}
+		}
+		return tagList;
 	}
 }
